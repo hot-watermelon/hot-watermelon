63ae97f96676471c3d1081e05c7b64cc451eb255
hot-watermelon <sdh220319@sdh.hs.kr>
2024-06-21 09:44:36 +0900

    ;)

---
+++ watermelon/fruits.js
@@ -0,0 +1,47 @@
+const FRUITS= [
+    {
+      name: "/watermelon/image/00_cherry",
+      radius: 33 / 2,
+    },
+    {
+      name: "/watermelon/image/01_strawberry",
+      radius: 48 / 2,
+    },
+    {
+      name: "/watermelon/image/02_grape",
+      radius: 61 / 2,
+    },
+    {
+      name: "/watermelon/image/03_gyool",
+      radius: 69 / 2,
+    },
+    {
+      name: "/watermelon/image/04_orange",
+      radius: 89 / 2,
+    },
+    {
+      name: "/watermelon/image/05_apple",
+      radius: 114 / 2,
+    },
+    {
+      name: "/watermelon/image/06_pear",
+      radius: 129 / 2,
+    },
+    {
+      name: "/watermelon/image/07_peach",
+      radius: 156 / 2,
+    },
+    {
+      name: "/watermelon/image/08_pineapple",
+      radius: 177 / 2,
+    },
+    {
+      name: "/watermelon/image/09_melon",
+      radius: 220 / 2,
+    },
+    {
+      name: "/watermelon/image/10_watermelon",
+      radius: 259 / 2,
+    },
+  ];
+  export { FRUITS };
--- watermelon/main.js
+++ watermelon/main.js
@@ -1,8 +1,11 @@
+import { FRUITS } from "/watermelon/fruits.js";
+
 var Engine = Matter.Engine;
 var Render = Matter.Render;
 var Runner = Matter.Runner;
 var Bodies = Matter.Bodies;
 var World = Matter.World;
+var Body = Matter.body;
 
 const engine = Engine.create();
 const render = Render.create({
@@ -15,6 +18,83 @@ const render = Render.create({
 		height: 850
 	}
 });
+const world = engine.world;
+
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+	isStatic: true,
+	render: {fillStyle: "#E6B143"}
+});
+const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+	isStatic: true,
+	render: {fillStyle: "#E6B143"}
+});
+const ground = Bodies.rectangle(310, 820, 620, 60, {
+	isStatic: true,
+	render: {fillStyle: "#E6B143"}
+});
+const topLine = Bodies.rectangle(310, 150, 620, 2, {
+	isStatic: true,
+	isSensor: true,
+	render: {fillStyle: "#E6B143"}
+});
+
+
+World.add(world, [leftWall, rightWall, ground, topLine]);
+
 
 Render.run(render);
 Runner.run(engine);
+
+let currentBody = null;
+let currentFruit = null;
+let disableAction = false;
+
+
+function addFruit() {
+	const index = Math.floor(Math.random() * 5);
+	const fruit = FRUITS[index];
+	const body = Bodies.circle(300, 50, fruit.radius, {
+		index: index,
+		isSleeping: true,
+		render: {
+			sprite: { texture: `${fruit.name}.png` }
+		},
+		restitution: 0.2
+	});
+
+	currentBody = body;
+	currentFruit = fruit;	
+
+	World.add(world, body);
+}
+
+
+window.onkeydown = (event) => {
+	if (disableAction)
+		return;
+	switch (event.code) {
+		case "KeyA":
+			Body.setPosition({
+				x: currentBody.position.x - 10,
+				y: currentBody.position.y
+			});
+			break;
+		case "KeyD":
+			Body.setPosition({
+				x: currentBody.position.x + 10,
+				y: currentBody.position.y
+			});
+			break;
+		case "KeyS":
+			currentBody.isSleeping = false;
+			disableAction = true;
+			setTimeout(() => {
+				addFruit();
+				disableAction = false;
+			}, 1000);
+			break;
+	}
+};
+
+
+addFruit();
